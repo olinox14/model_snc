15dc87f2e890ea168c38e39b8bc37158110caf01
Olivier Massot <olivier.massot@2iopenservice.fr>
2025-06-06 11:37:42 +0200

    update about page

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -5,6 +5,9 @@
       <p class="text-center">
         This is a demo application built with Symfony 7 + Nuxt.js 3.
       </p>
+      <p class="text-center">
+        <a href="https://github.com/olinox14/model_snc">Contribute</a>
+      </p>
     </v-card>
   </div>
 </template>
